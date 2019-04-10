e34861e3ef5608bed9d4f00944c3332fd17d637a
Jose Maria Gallego <josema.g.m.95@hotmail.com>
2019-04-10 20:56:18 +0200

    Apuntes sobre PIPES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Jose';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a:number = 0.234;
+  salario:number = 1234.5;
 }
